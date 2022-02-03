33de927aca5156672441460a9e59ee71dc6105bf
Kyle Hanson <khanson1029@gmail.com>
2022-02-03 09:38:05 -0700

    HTML/CSS initial skeleton

---
+++ header.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Page Title</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="main.css">
+<link href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" rel="stylesheet">
+<style>
+</style>
+<script src=""></script>
+<body>
+
+<div class="main-grid-container">
+    <div class="banner">
+        <h1 id="logo-main">LOW KEY LOVE</h1>
+       <!-- <a class="logo-main" href="http://localhost.com">LearnLowKey</a> -->
+    </div>
+    <div class="nav-container">
+        <nav class="navbar" role="navigation">
+            <ul class="nav-items">
+                <a href="index.php", class="navitem1">HOME</a>                
+                <a href="listen.php", class="navitem2">LISTEN</a>
+                <a href="learn.php", class="navitem3">LEARN</a>
+                <li class="dropdown">
+                    <a href="javascript:void(0)" class="dropbtn">
+                        <i class="far fa-user"></i>
+                    </a>
+                    <div class="dropdown-content"> 
+                        <a href="login.php", class="droplist">Login</a>
+                        <a href="getinvolved.php", class="droplist">Get Involved</a>
+                    </div> 
+                </li>
+                
+            </ul>
+        </nav>
+    </div>   
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php require_once 'header.php'; ?>
+    <div class="highlight-posts">
+        <section role="main">
+            <article id="post-of-the-day">
+                <header>
+                    <h1 class="post-title">
+                        <a href="learn.html" title="Song of the Day">
+                            Song of the Day
+                        </a>
+                    </h1>
+                </header>
+            </article>
+        </section>
+    </div> 
+</div>
+<footer class="footer" role="contentinfo"></footer>
+
+</body>
+</html> 
+
+
+
