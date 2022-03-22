f65c97bc31610f11318543778bce93f5cbe85a07
Kyle Hanson <khanson1029@gmail.com>
2022-03-21 20:15:57 -0600

    login changes

--- login_handler.php
+++ login_handler.php
@@ -1,23 +1,23 @@
 <!-- <?php
-    // login_handler.php
-    session_start();
-    require_once("Dao.php");
-    if(isset($_POST["userSubmitButton"])){
-      $username = $_POST["username"];
-      $password = hash("sha256", $_POST['password'] . "fKd93Vmz!k*dAv5029Vkf9$3Aa");
-    }
+    // // login_handler.php
+    // session_start();
+    // require_once("Dao.php");
+    // if(isset($_POST["userSubmitButton"])){
+    //   $username = $_POST["username"];
+    //   $password = hash("sha256", $_POST['password'] . "fKd93Vmz!k*dAv5029Vkf9$3Aa");
+    // }
 
-    $dao = new Dao();
-    $_SESSION['authenticated'] = $dao->userExists($username, $password);
+    // $dao = new Dao();
+    // $_SESSION['authenticated'] = $dao->userExists($username, $password);
 
-    if($_SESSION['authenticated']){
-      header("Location:welcome.php");
-      exit;
-    }else{
-      $_SESSION['authenticated'] = false;
-      header("Location:login.php");
-      exit;
-    }
+    // if($_SESSION['authenticated']){
+    //   header("Location:welcome.php");
+    //   exit;
+    // }else{
+    //   $_SESSION['authenticated'] = false;
+    //   header("Location:login.php");
+    //   exit;
+    // }
 ?>  -->
 <?php
 // login_handler.php
