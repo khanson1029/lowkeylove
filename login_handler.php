135e90de53526fe9085dec9ecbbbf009ae8c19b7
Kyle Hanson <khanson1029@gmail.com>
2022-03-21 21:19:54 -0600

    login changes

--- login_handler.php
+++ login_handler.php
@@ -1,36 +1,36 @@
-<!-- <?php
+<?php
     // // login_handler.php
-    // session_start();
-    // require_once("Dao.php");
-    // if(isset($_POST["userSubmitButton"])){
-    //   $username = $_POST["username"];
-    //   $password = hash("sha256", $_POST['password'] . "fKd93Vmz!k*dAv5029Vkf9$3Aa");
-    // }
+    session_start();
+    require_once("Dao.php");
+    if(isset($_POST["userSubmitButton"])){
+      $username = $_POST["username"];
+      $password = hash("sha256", $_POST['password'] . "fKd93Vmz!k*dAv5029Vkf9$3Aa");
+    }
 
-    // $dao = new Dao();
-    // $_SESSION['authenticated'] = $dao->userExists($username, $password);
+    $dao = new Dao();
+    $_SESSION['authenticated'] = $dao->userExists($username, $password);
 
-    // if($_SESSION['authenticated']){
-    //   header("Location:welcome.php");
-    //   exit;
-    // }else{
-    //   $_SESSION['authenticated'] = false;
-    //   header("Location:login.php");
-    //   exit;
-    // }
-?>  -->
+    if($_SESSION['authenticated']){
+      header("Location:welcome.php");
+      exit;
+    }else{
+      $_SESSION['authenticated'] = false;
+      header("Location:login.php");
+      exit;
+    }
+?> 
 <?php
 // login_handler.php
-session_start();
-$dao = new Dao();
-// For simplification Lets pretend I got these login credentials from an SQL table.
-if ($dao->userExists($_POST["username"], $_POST["password"])) {
-  $_SESSION["authenticated"] = true;
-  header("Location:welcome.php");
+// session_start();
+// $dao = new Dao();
+// // For simplification Lets pretend I got these login credentials from an SQL table.
+// if ($dao->userExists($_POST["username"], $_POST["password"])) {
+//   $_SESSION["authenticated"] = true;
+//   header("Location:welcome.php");
 
-} else {
-  $_SESSION["authenticated"] = false;
+// } else {
+//   $_SESSION["authenticated"] = false;
 
-  header("Location:login.php");
-}
+//   header("Location:login.php");
+// }
 ?> 
