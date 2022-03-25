b26cf3783586b1b9a1fddedf0766065202b628df
Kyle Hanson <khanson1029@gmail.com>
2022-03-25 14:59:08 -0600

    organizational changes

--- comments.php
+++ comments.php
@@ -23,23 +23,26 @@
 <div class="comment-box">
     <div class="dialogbox">
         <div class="body">
-        <!-- <span class="tip tip-up"></span>
-                    <div class="message">LEAVE NOW!
- 
-                    </div>
-                </span> -->
-            <?php
-                $dao = new Dao();
-                $comments = $dao->getComments();
-                foreach ($comments as $comment) {
-                    echo "<span class='tip tip-up'>";
-                    echo        "<div class = 'message'>";
-                    echo            "<span>" . $comment["comment"] . "</span>";
-                    echo        "</div>";
-                    echo "</span>";
+            <span class="tip tip-up"></span>
+                <div class="message">LEAVE NOW!
+                </div>
+            </span>
+            <span class="tip tip-up"></span>
+                <div class="message">LEAVE NOW!
+                </div>
+            </span>
+            <!-- <?php
+                // $dao = new Dao();
+                // $comments = $dao->getComments();
+                // foreach ($comments as $comment) {
+                //     echo "<span class='tip tip-up'>";
+                //     echo        "<div class = 'message'>";
+                //     echo            "<span>" . $comment["comment"] . "</span>";
+                //     echo        "</div>";
+                //     echo "</span>";
                     // . - $comment["username"] . "</span>";
-                }
-            ?>
+              //  }
+            ?> -->
         </div>
     </div>
 </div>
