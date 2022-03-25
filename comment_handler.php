17d01ea2512e0208af0e0eef35aed8668f2b40ef
Kyle Hanson <khanson1029@gmail.com>
2022-03-25 16:54:35 -0600

    organizational changes

--- comment_handler.php
+++ comment_handler.php
@@ -5,13 +5,19 @@ session_start();
 require_once "Dao.php";
 
   if (isset($_POST["commentButton"])){
-    $comment = $_POST["comment"];
-    try {
-      $dao = new Dao();
-      $dao->saveComment($comment);
-    } catch (Exception $e) {
-      var_dump($e);
-      die;
+    if($_SESSION['authenticated'] === true){
+      $comment = $_POST["comment"];
+      try {
+        $dao = new Dao();
+        $dao->saveComment($comment);
+      } catch (Exception $e) {
+        var_dump($e);
+        die;
+      }
+      header("Location:index.php");
+    }else{
+      header("Location:login.php");
     }
+
    }
-  header("Location:index.php");
+  
