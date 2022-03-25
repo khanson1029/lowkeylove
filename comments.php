5b038274b5014ff411e34b145ec198c071e23bf6
Kyle Hanson <khanson1029@gmail.com>
2022-03-25 15:30:47 -0600

    organizational changes

--- comments.php
+++ comments.php
@@ -29,7 +29,7 @@
                         $dao = new Dao();
                         $comments = $dao->getComments();
                         foreach ($comments as $comment) {
-                            echo "<span>" . $comment["comment"] . "</span>";
+                            echo "<p>" . $comment["comment"] . "</p>";
                             // . - $comment["username"] . "</span>";
                         }
                     ?>
