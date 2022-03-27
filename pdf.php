f6b8c156852324d9eae69b507efccdcf10fefb89
Kyle Hanson <khanson1029@gmail.com>
2022-03-27 11:26:05 -0600

    my account changes

--- pdf.php
+++ pdf.php
@@ -8,13 +8,48 @@ $products = $dao->getPdfs();
 
 ?>
 <form name="loginForm" action="login_handler.php" method="POST">
-    <div id="user-container">
-        <input type="text" name="username" , placeholder="Song Name">
-    </div>
+    <!-- <div id="user-container">
+        <input type="text" name="songname" , placeholder="Song Name">
+    </div> -->
     <div id>
-        <input type="text" name="password" , placeholder="Description">
+        <input type="file" name="file" , size = "50">
     </div>
     <div class="submit-button">
         <input type="submit" name="userSubmitButton" value="Submit">
     </div>
 </form>
+<?php
+
+$targetfolder = "pdfcontent/";
+
+$targetfolder = $targetfolder . basename( $_FILES['file']['name']) ;
+
+$ok=1;
+
+$file_type=$_FILES['file']['type'];
+
+if ($file_type=="application/pdf") {
+
+if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder))
+
+{
+
+echo "The file ". basename( $_FILES['file']['name']). " is uploaded";
+
+}
+
+else {
+
+echo "Problem uploading file";
+
+}
+
+}
+
+else {
+
+echo "You may only upload PDFs.<br>";
+
+}
+
+?>
