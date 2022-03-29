37b1b420c6a5aac2069c02e59276ad90dbe0ba15
Kyle Hanson <khanson1029@gmail.com>
2022-03-28 19:28:05 -0600

    my account changes

--- pdf_handler.php
+++ pdf_handler.php
@@ -18,9 +18,9 @@
       throw new Exception("Error: " . $_FILES["pdffile"]["error"]);
     } else {
       $basePath = "/var/www/cs401_domain";
-      echo print_r($_FILES, 1);
-      exit;
-      $imagePath = "/pdfcontent" . $_FILES["pdffile"]["pdfname"];
+    //   echo print_r($_FILES, 1);
+    //   exit;
+      $imagePath = "/pdfcontent" . $_FILES["pdffile"]["name"];
       if (!move_uploaded_file($_FILES["pdffile"]["tmp_name"], $basePath . $imagePath)) {
         throw new Exception("File move failed");
       }
