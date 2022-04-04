3233fce51a14661eb12fa0c342ffb0fe99c129d0
Kyle Hanson <khanson1029@gmail.com>
2022-04-04 16:15:11 -0600

    mp3 handler update

--- listen.php
+++ listen.php
@@ -56,7 +56,7 @@
             foreach($files as $file){?>
                 <li class="active" id="<?php $i;?>"> 
                     <a class="mp3-listen-object-container" href="
-                        <?php print(@$files[$i]); ?>">
+                        <?php print($files); ?>">
                         Sorry, your browser isn't compatible with this mp3 viewer.
                     </a>
                 </li>
