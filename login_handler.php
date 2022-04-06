e9fddd4ab58282f4d7c21c0c5691378fc0144bc7
Kyle Hanson <khanson1029@gmail.com>
2022-04-06 16:51:08 -0600

    songnames

--- login_handler.php
+++ login_handler.php
@@ -7,10 +7,10 @@
       $password = hash("sha256", $_POST['password'] . "fKd93Vmz!k*dAv5029Vkf9$3Aa");
       $_SESSION['username'] = $username;
     }
-    echo "hi";
+    
     $dao = new Dao();
     $_SESSION['authenticated'] = $dao->userExists($username, $password);
-
+    echo "hi";
     if($_SESSION['authenticated']){
       header("Location:/app/myaccount.php");
       exit;
